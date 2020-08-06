d6a9cbd58db367b94285c0b5af8e2625cb9cb8bc
marwinburesch <marwin@posteo.de>
2020-08-06 14:46:28 +0200

    Add add function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "./global.css";
 import { createButton } from "./components/button.js";
+import { add } from "./exercises/add";
 
-const addButton = createButton("Add two numbers", () => console.log("added"));
+const addButton = createButton("Add two numbers", () => console.log(add(4, 5)));
 
 document.body.append(addButton);
