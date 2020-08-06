332033bfdf3d866b544b35ea2fc751cf1e5a590c
marwinburesch <marwin@posteo.de>
2020-08-06 17:03:22 +0200

    Rename parameter

--- src/components/button.js
+++ src/components/button.js
@@ -1,15 +1,13 @@
 import "./button.css";
 import { createElement } from "../utils/manipulateDom.js";
 
-export function createButton(text, callbackFunction) {
+export function createButton(text, onClick) {
   const button = createElement("button", {
     className: "button_primary",
     innerHTML: text,
   });
 
-  button.addEventListener("click", () => {
-    callbackFunction();
-  });
+  button.addEventListener("click", onClick);
 
   return button;
 }
