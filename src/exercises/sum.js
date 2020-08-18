2d32d07415c67917d812f154055771d6d244c5d7
marwinburesch <marwin@posteo.de>
2020-08-18 11:12:53 +0200

    Add more sum solutions

--- src/exercises/sum.js
+++ src/exercises/sum.js
@@ -1,4 +1,23 @@
-// export default function sumAlternative(numbers) {
+export default function sum(numbers) {
+  // declare a variable that can be reassigned within this scope
+  let result = 0;
+  // forEach runs over the incoming array (numbers)
+  numbers.forEach((number) => {
+    result = number + result;
+  });
+  return result;
+}
+
+// This is the same function but written differently
+// export default function sum(numbers) {
+//   let result = 0;
+//   const add = (number) => (result = number + result);
+//   numbers.forEach(add);
+//   return result;
+// }
+
+// ALTERNATIVES
+// export default function sum(numbers) {
 //   let result = 0;
 //   for (let index = 0; index < numbers.length; index++) {
 //     const element = numbers[index];
@@ -7,15 +26,8 @@
 //   return result;
 // }
 
-// export default function sumReducer(numbers) {
-//   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-//   return numbers.reduce(reducer);
+// export default function sum(numbers) {
+//   return numbers.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue
+//   );
 // }
-
-export default function sum(numbers) {
-  let result = 0;
-  numbers.forEach((number) => {
-    result = number + result;
-  });
-  return result;
-}
